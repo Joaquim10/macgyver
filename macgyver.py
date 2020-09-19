f348e81bd69aceb84aed7dcda79fa05fcfbaf47f
Joaquim10 <jodossantos@free.fr>
2020-09-19 23:51:42 +0200

    manage MacGyver class

--- macgyver.py
+++ macgyver.py
@@ -73,6 +73,13 @@ class MacGyver:
 
     def __init__(self, position):
         self.position = position
+        self.items_in_backpack = 0
+
+    def move(self, position):
+        self.position = position
+
+    def pick_up_item(self):
+        self.items_in_backpack += 1
 
 
 class Guardian:
