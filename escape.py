02e846c56e27baf16980a62b49ad22245419cf0f
Joaquim10 <jodossantos@free.fr>
2020-09-27 05:09:01 +0200

    add centered surface to display maze and end game

--- escape.py
+++ escape.py
@@ -18,23 +18,24 @@ class Game:
         "move right": pygame.K_RIGHT,
         "move up": pygame.K_UP,
         "move down": pygame.K_DOWN,
-        "exit game" : pygame.K_ESCAPE
+        "exit game": pygame.K_ESCAPE,
+        "ok": pygame.K_SPACE
     }
 
     ENDINGS = {
         "game won" :
-            "The guardian bars the exit of the maze.\n"
-            "MacGyver sneaks into the shadows and\n"
-            "plants his anaesthetic syringe\n"
-            "in the guard's neck, who collapses.\n"
-            "MacGyver then escape the labyrinth.\n"
+            "The guardian bars the exit of the maze.\n\n"
+            "MacGyver sneaks into the shadows\n"
+            "and plants his anaesthetic syringe\n"
+            "in the guard's neck, who collapses.\n\n"
+            "MacGyver then escape the labyrinth.\n\n"
             "Well played !",
         "game lost":
             "MacGyver rushes the exit of the maze,\n"
-            "but he's killed by the Guardian.\n"
+            "but he's killed by the Guardian.\n\n"
             "Game over !",
         "game canceled":
-            "MacGyver is lost in the maze forever.\n"
+            "MacGyver is lost in the maze forever.\n\n"
             "Game over !"
     }
 
@@ -43,17 +44,25 @@ class Game:
         pygame.init()
         self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
         pygame.display.set_caption(self.CAPTION)
-        # Scale images
-        Image.WIDTH = Image.HEIGHT = min(self.SCREEN_WIDTH // Maze.WIDTH, self.SCREEN_HEIGHT // Maze.HEIGHT)
-        # Center screen
-        self.maze_x = (self.SCREEN_WIDTH - Maze.WIDTH * Image.WIDTH) // 2
-        self.maze_y = (self.SCREEN_HEIGHT - Maze.HEIGHT * Image.HEIGHT) // 2
+        # Initialize scaled images
+        Image.WIDTH = Image.HEIGHT = min(self.SCREEN_WIDTH // Maze.WIDTH,
+            self.SCREEN_HEIGHT // Maze.HEIGHT)
+        # Initialize screen centered maze area
+        self.maze_area, self.maze_rect = self.maze_area()
         # Initialize game
-        self.game_status = "game in progress"
         self.maze = Maze()
         self.macgyver = MacGyver()
         self.guardian = Guardian()
         self.items = Items()
+
+    def maze_area(self):
+        width = Maze.WIDTH * Image.WIDTH
+        height = Maze.HEIGHT * Image.HEIGHT
+        x_coordinate = (self.SCREEN_WIDTH - width) // 2
+        y_coordinate = (self.SCREEN_HEIGHT - height) // 2
+        maze_rect = pygame.Rect(x_coordinate, y_coordinate, width, height)
+        maze_area = self.screen.subsurface(maze_rect).convert()
+        return maze_area, maze_rect
 
     @classmethod
     def command(cls, key):
@@ -84,6 +93,7 @@ class Game:
         y_coordinate < Maze.MIN_HEIGHT or y_coordinate > Maze.MAX_HEIGHT)
 
     def move(self, destination):
+        game_status = "game in progress"
         self.macgyver.move(destination)
         for item in Items.loot:
             if destination == item.position:
@@ -92,56 +102,111 @@ class Game:
                     self.items.craft()
         if destination == self.guardian.position:
             if self.items.syringe in Items.backpack:
-                self.game_status = "game won"
+                game_status = "game won"
             else:
-                self.game_status = "game lost"
+                game_status = "game lost"
+        return game_status
 
-    def display_screen(self):
-        # Display background
+    def blit_maze_area(self):
+        # Display maze background
         for y_coordinate in range(Maze.MIN_HEIGHT, Maze.HEIGHT):
             for x_coordinate in range(Maze.MIN_WIDTH, Maze.WIDTH):
                 if (Maze.zones[x_coordinate, y_coordinate] == Maze.WALL):
-                    self.maze.wall_rect.left = x_coordinate * self.maze.wall_rect.w
-                    self.maze.wall_rect.top = y_coordinate * self.maze.wall_rect.h
-                    self.screen.blit(self.maze.wall_image, self.maze.wall_rect.move(self.maze_x, self.maze_y))
+                    self.maze.wall_rect.x = x_coordinate * self.maze.wall_rect.width
+                    self.maze.wall_rect.y = y_coordinate * self.maze.wall_rect.height
+                    self.maze_area.blit(self.maze.wall_image, self.maze.wall_rect)
                 else:
-                    self.maze.path_rect.left = x_coordinate * self.maze.path_rect.w
-                    self.maze.path_rect.top = y_coordinate * self.maze.path_rect.h
-                    self.screen.blit(self.maze.path_image, self.maze.path_rect.move(self.maze_x, self.maze_y))
+                    self.maze.path_rect.x = x_coordinate * self.maze.path_rect.width
+                    self.maze.path_rect.y = y_coordinate * self.maze.path_rect.height
+                    self.maze_area.blit(self.maze.path_image, self.maze.path_rect)
         # Display sprites
         for item in self.items.loot:
-            self.screen.blit(item.image, item.rect.move(self.maze_x, self.maze_y))
-        self.screen.blit(self.guardian.image, self.guardian.rect.move(self.maze_x, self.maze_y))
-        self.screen.blit(self.macgyver.image, self.macgyver.rect.move(self.maze_x, self.maze_y))
-        pygame.display.flip()
+            self.maze_area.blit(item.image, item.rect)
+        self.maze_area.blit(self.guardian.image, self.guardian.rect)
+        self.maze_area.blit(self.macgyver.image, self.macgyver.rect)
+
+    def blit_ending(self, ending, position, font, color=pygame.Color("white")):
+        # 2D array where each row is a list of words
+        words = [word.split(' ') for word in ending.splitlines()]
+        space_width = font.size(' ')[0]
+        x_coordinate, y_coordinate = position
+        for line in words:
+            for word in line:
+                word_surface = font.render(word, 0, color)
+                word_width, word_height = word_surface.get_size()
+                if x_coordinate + word_width >= self.maze_rect.right - position[0]:
+                    x_coordinate = position[0]  # New line
+                    y_coordinate += word_height
+                self.maze_area.blit(word_surface, (x_coordinate, y_coordinate))
+                x_coordinate += word_width + space_width
+            x_coordinate = position[0]  # New line
+            y_coordinate += word_height
+
+    def blit_message(self, message, y_position, font, color=pygame.Color("white")):
+        message_surface = font.render(message, 0, color)
+        message_rect = message_surface.get_rect()
+        message_rect.x = (self.maze_rect.width - message_rect.width) // 2
+        message_rect.y = self.maze_rect.bottom - message_rect.height - y_position
+        self.maze_area.blit(message_surface, message_rect)
+
 
     def play_game(self):
+        game_status = "game in progress"
         dirty_rects = []
-        self.display_screen()
+        self.blit_maze_area()
+        self.screen.blit(self.maze_area, self.maze_rect)
+        pygame.display.flip()
         #Output.print_interface(self.macgyver.position, self.items.items_in_backpack)
         # Event loop
-        while self.game_status == "game in progress":
+        while game_status not in ["game canceled", "game over"]:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    self.game_status = "game canceled"
+                    game_status = "game canceled"
                 elif event.type == pygame.KEYDOWN:
                     command = self.command(event.key)
-                    if command == "exit game":
-                        self.game_status = "game canceled"
-                    elif command.startswith("move"):
+                    if (command == "exit game" and game_status != "game won" and
+                    game_status != "game lost"):
+                        game_status = "game canceled"
+                    if command in ["exit game", "ok"] and game_status in ["game won", "game lost"]:
+                        game_status = "game over"
+                    elif command.startswith("move") and game_status == "game in progress":
                         destination = self.destination(command)
                         if not self.collision_detected(destination):
-                            self.screen.blit(self.maze.path_image, self.macgyver.rect.move(self.maze_x, self.maze_y))
-                            dirty_rects.append(self.macgyver.rect.copy().move(self.maze_x, self.maze_y))
-                            self.move(destination)
-                            self.screen.blit(self.maze.path_image, self.macgyver.rect.move(self.maze_x, self.maze_y))
-                            self.screen.blit(self.macgyver.image, self.macgyver.rect.move(self.maze_x, self.maze_y))
-                            dirty_rects.append(self.macgyver.rect.move(self.maze_x, self.maze_y))
+                            # MacGyver moves
+                            self.screen.blit(self.maze.path_image, self.macgyver.rect.move(
+                                self.maze_rect.x, self.maze_rect.y))
+                            dirty_rects.append(self.macgyver.rect.move(
+                                self.maze_rect.x, self.maze_rect.y))
+                            game_status = self.move(destination)
+                            self.screen.blit(self.maze.path_image, self.macgyver.rect.move(
+                                self.maze_rect.x, self.maze_rect.y))
+                            if game_status == "game lost":
+                                self.screen.blit(self.guardian.image, self.macgyver.rect.move(
+                                    self.maze_rect.x, self.maze_rect.y))
+                            else:
+                                self.screen.blit(self.macgyver.image, self.macgyver.rect.move(
+                                    self.maze_rect.x, self.maze_rect.y))
+                            dirty_rects.append(self.macgyver.rect.move(
+                                self.maze_rect.x, self.maze_rect.y))
                             pygame.display.update(dirty_rects)
                             dirty_rects.clear()
                             #Output.print_interface(self.macgyver.position, self.items.items_in_backpack)
-        Output.print_ending(self.ENDINGS[self.game_status]) # Ending
-
+                            if game_status in ["game won", "game lost"]:
+                                # Display screen centered ending
+                                self.maze_area = self.screen.subsurface(self.maze_rect).convert()
+                                font = pygame.font.SysFont('Arial', Image.WIDTH * 5 // 6)
+                                ending = (self.ENDINGS[game_status] + "\n\n")
+                                message = "Press SPACE or ESCAPE to quit."
+                                self.blit_ending(ending, (Image.WIDTH // 2, Image.HEIGHT),
+                                    font, pygame.Color('white'))
+                                self.blit_message(message, Image.HEIGHT * 2, font,
+                                    pygame.Color("orange"))
+                                self.screen.blit(self.maze_area, self.maze_rect)
+                                pygame.display.flip()
+                                Output.print_ending(self.ENDINGS[game_status])
+        # Game is over
+        if game_status == "game canceled":
+            Output.print_ending(self.ENDINGS[game_status])
 
 def main():
     game = Game()
