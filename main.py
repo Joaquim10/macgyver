0c28c88c68b460bcd3261303b01c4267f9a88ccc
Joaquim10 <jodossantos@free.fr>
2020-10-07 15:28:18 +0200

    add main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/bin/python/env python3
+# -*- coding: UTF-8 -*-
+"""The main module runs the game."""
+
+from app.escape import Escape
+
+
+if __name__ == "__main__":
+    escape = Escape()
+    escape.run()
